4b7c669ac9adb19ba6aa7b5530dac8725f4b00e3
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-11-16 13:39:04 -0500

    Adding main to ex2

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -6,7 +6,7 @@
 
 char **reconstruct_trip(Ticket **tickets, int length)
 {
-  HashTable *hash = create_hash_table(16);
+  HashTable *ht = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
@@ -20,3 +20,32 @@ void print_route(char **route, int length)
     printf("%s\n", route[i]);
   }
 }
+
+
+
+#ifndef TESTING
+int main(void)
+{
+  // Short test
+  Ticket **tickets = malloc(3 * sizeof(Ticket *));
+
+  Ticket *ticket_1 = malloc(sizeof(Ticket));
+  ticket_1->source = "NONE";
+  ticket_1->destination = "PDX";
+  tickets[0] = ticket_1;
+
+  Ticket *ticket_2 = malloc(sizeof(Ticket));
+  ticket_2->source = "PDX";
+  ticket_2->destination = "DCA";
+  tickets[1] = ticket_2;
+
+  Ticket *ticket_3 = malloc(sizeof(Ticket));
+  ticket_3->source = "DCA";
+  ticket_3->destination = "NONE";
+  tickets[2] = ticket_3;
+
+  print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
+
+  return 0;
+}
+#endif
