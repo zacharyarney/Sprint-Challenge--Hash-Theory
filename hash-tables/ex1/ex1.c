491034642aabe2ba0fac51ece290be63afe58c88
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-11-16 13:35:34 -0500

    Adding main to ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -20,3 +20,31 @@ void print_answer(Answer *answer)
     printf("NULL\n");
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+
+  // TEST 1
+  int weights_1 = {9};
+  Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
+  print_answer(answer_1);  // NULL
+
+  // TEST 2
+  int weights_2[] = {4, 4};
+  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  print_answer(answer_2);  // {1, 0}
+
+  // TEST 3
+  int weights_3[] = {4, 6, 10, 15, 16};
+  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  print_answer(answer_3);  // {3, 1}
+
+  // TEST 4
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  print_answer(answer_4);  // {6, 2}
+
+  return 0;
+}
+#endif
