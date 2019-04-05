19170ebb207ac2ce5f1ab009e0a6e6acb0c8daf7
zacharyarney <zachary.arney@gmail.com>
2019-04-05 10:25:32 -0700

    ex2 passing

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -6,19 +6,29 @@
 
 char **reconstruct_trip(Ticket **tickets, int length)
 {
-  HashTable *ht = create_hash_table(16);
-  char **route = malloc(length * sizeof(char *));
+    HashTable *ht = create_hash_table(16);
+    char **route = malloc(length * sizeof(char *));
 
-  // YOUR CODE HERE
+    // populate hash table with flight data
+    for (int i = 0; i < length; i++) {
+        hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+    }
+    // initialize route with starting city (source = "NONE)
+    route[0] = hash_table_retrieve(ht, "NONE");
+    for (int i = 1; i < length; i++) {
+        // make current index in route array equal to the value of the previous
+        // this matches destination of prev flight to source of current.
+        route[i] = hash_table_retrieve(ht, route[i-1]);
+    }
 
-  return route;
+    return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
-    printf("%s\n", route[i]);
-  }
+    for (int i = 0; i < length; i++) {
+        printf("%s\n", route[i]);
+    }
 }
 
 
@@ -26,26 +36,26 @@ void print_route(char **route, int length)
 #ifndef TESTING
 int main(void)
 {
-  // Short test
-  Ticket **tickets = malloc(3 * sizeof(Ticket *));
+    // Short test
+    Ticket **tickets = malloc(3 * sizeof(Ticket *));
 
-  Ticket *ticket_1 = malloc(sizeof(Ticket));
-  ticket_1->source = "NONE";
-  ticket_1->destination = "PDX";
-  tickets[0] = ticket_1;
+    Ticket *ticket_1 = malloc(sizeof(Ticket));
+    ticket_1->source = "NONE";
+    ticket_1->destination = "PDX";
+    tickets[0] = ticket_1;
 
-  Ticket *ticket_2 = malloc(sizeof(Ticket));
-  ticket_2->source = "PDX";
-  ticket_2->destination = "DCA";
-  tickets[1] = ticket_2;
+    Ticket *ticket_2 = malloc(sizeof(Ticket));
+    ticket_2->source = "PDX";
+    ticket_2->destination = "DCA";
+    tickets[1] = ticket_2;
 
-  Ticket *ticket_3 = malloc(sizeof(Ticket));
-  ticket_3->source = "DCA";
-  ticket_3->destination = "NONE";
-  tickets[2] = ticket_3;
+    Ticket *ticket_3 = malloc(sizeof(Ticket));
+    ticket_3->source = "DCA";
+    ticket_3->destination = "NONE";
+    tickets[2] = ticket_3;
 
-  print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
+    print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
 
-  return 0;
+    return 0;
 }
 #endif
